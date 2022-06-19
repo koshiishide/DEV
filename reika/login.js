acb3f8b76605d1cc5968bfc43a6193bba79f5657
rerereika <107592822+rerereika@users.noreply.github.com>
2022-06-19 21:53:09 +0900

    ログイン画面
    HELP

---
+++ reika/login.js
@@ -0,0 +1,10 @@
+var id;
+var pass;
+
+var button = document.getElementById("login");
+button.addEventListener("click",signin);
+function signin(){
+    var element = document.createElement("p");
+    var text = document.createElement= ("IDかパスワードに間違いがあります");
+    document.body.appendChild(element).appendChild(text);
+}
